f0aa0767be24544eb83cf3ac208eaad6a28320e7
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-12-24 01:57:27 +0530

    wrote sign in route added types and configured db

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { router } from "./router/v1";
+import client from "@repo/db/client" ;
 
 const app = express() ;
+app.use(express.json()) ;
 
 app.use("/api/v1",router);
 
--- apps/http/src/router/v1/index.ts
+++ apps/http/src/router/v1/index.ts
@@ -1,18 +1,56 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
+import { SigninSchema, SignupSchema } from "../../types";
+import client from "@repo/db/client" ;
+import bcrypt from "bcrypt" ;
 
 export const router = Router() ;
 
-router.post('/signup',(req,res)=>{
-    res.send(`<h1>hey babe</h1>`)
+router.post('/signup',async(req:Request,res:Response)=>{
+    const parsedData = SignupSchema.safeParse(req.body) ;
+    if(!parsedData.success){
+        res.status(404).json({message:"Validation failed"}) ;
+        return ;
+    }
+
+    const hashedPassword = await bcrypt.hash(parsedData.data.password,10) ;
+    try {
+        const user = await client.user.create({
+            data:{
+                username:parsedData.data.username,
+                password:hashedPassword,
+                role:parsedData.data.type==="admin"?"Admin":"User"
+            }
+        })
+
+        res.status(200).json({
+            userId:user.id,
+            message:"You have successfully created your account"
+        })
+    } catch (error) {
+        res.status(404).json({message:"Username already exixts"})
+    }
 })
 
-router.post("/signin",(req,res)=>{
-    res.json({
-        message:"Signin"
-    })
+router.post("/signin",async(req,res)=>{
+    const parsedData = SigninSchema.safeParse(req.body) ;
+    if(!parsedData.success){
+        res.send(403).json({
+            message:"Validation Failed"
+        })
+    }
+
+    try{
+        // const user = await client.user.findUnique({
+        //     where:{
+        //         username:parsedData.data.username
+        //     }
+        // })
+    }catch(e){
+
+    }
 })
 
 router.get("/elements",(req,res)=>{
